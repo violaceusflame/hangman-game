06f4aa3d9130680518b5730b0409ecc66cecadaf
Alexander <lushmetry@gmail.com>
2024-08-20 16:59:48 +0300

    Fixed resource search
    Now it should work on other PCs

--- src/main/java/io/github/violaceusflame/repository/WordRepository.java
+++ src/main/java/io/github/violaceusflame/repository/WordRepository.java
@@ -4,23 +4,35 @@ import io.github.violaceusflame.session.HiddenWord;
 import io.github.violaceusflame.validator.WordRepositoryValidator;
 
 import java.io.*;
+import java.net.URL;
 import java.util.List;
 
+// TODO: Путь к файлу должен передаваться в конструктор, а не быть жестко прошитым внутри класса
 public class WordRepository {
+    // TODO: Полагаю, здесь тоже имеет некоторая зависимость от представления.
     public static final String FILE_READ_ERROR = "Ошибка при чтении файла со списком слов";
     private static final String PATH_TO_WORDS_FILE = "src/main/resources/words.txt";
+    private static final WordRepositoryValidator wordRepositoryValidator = new WordRepositoryValidator();
     private static List<String> words;
 
     private static void loadWords() {
-        try (var reader = new BufferedReader(new FileReader(PATH_TO_WORDS_FILE))) {
+        try (var reader = new BufferedReader(new FileReader(getFileResource()))) {
             words = reader.lines()
                     .map(String::trim)
                     .filter(s -> !s.isEmpty())
                     .toList();
-            WordRepositoryValidator.validate(words);
+            wordRepositoryValidator.validate(words);
         } catch (IOException e) {
             throw new RuntimeException(FILE_READ_ERROR, e);
         }
+    }
+
+    private static File getFileResource() {
+        URL resource = WordRepository.class.getResource("/words.txt");
+        if (resource == null) {
+            throw new RuntimeException(FILE_READ_ERROR);
+        }
+        return new File(resource.getFile());
     }
 
     public static HiddenWord get() {
